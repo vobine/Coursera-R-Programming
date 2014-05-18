38eb169c21f6dd9e3fec43a10e0a6cd57e74e766
Hal Peterson <vobine@users.noreply.github.com>
2014-05-18 00:58:19 -0500

    Actual first function

--- project-1/pollutantmean.R
+++ project-1/pollutantmean.R
@@ -1,3 +1,12 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  list.files (directory)
+  dataSum = 0
+  dataCount = 0
+  for (i in id) {
+    data = read.csv (file.path (directory, sprintf ('%03d.csv', i)))
+    dataMask = ! is.na (data[, pollutant])
+    dataSum = dataSum + sum (data[dataMask, pollutant])
+    dataCount = dataCount + sum (dataMask)
+  }
+  
+  return (dataSum / dataCount)
 }
