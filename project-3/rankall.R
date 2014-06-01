e68616076d3638fb740040ceb256904ff92faefd
Hal Peterson <vobine@users.noreply.github.com>
2014-06-01 12:43:08 -0500

    Apply across states

--- project-3/rankall.R
+++ project-3/rankall.R
@@ -12,24 +12,30 @@ rankall <- function(outcome, num='best') {
     stop ('invalid outcome');
   }
 
-  ## Convert state and outcome columns
-  outcomes$State <- as.factor (outcomes$State)
+  ## Convert outcome columns
   outcomes[, outcomeCol] <- as.numeric (outcomes[, outcomeCol]);
 
-  ## Rank outcomes
-  ranks <- order (outcomes[, outcomeCol], 
-                  as.character (outcomes$Hospital.Name), 
-                  na.last=NA)
+  ## Ranking function for each state
+  rankEach <- function (data) {
+    ## Rank outcomes
+    ranks <- order (data[, outcomeCol], 
+                    as.character (data$Hospital.Name), 
+                    na.last=NA)
+    
+    ## Convert special-case rank values
+    if (num == 'best') {
+      index <- 1
+    } else if (num == 'worst') {
+      index <- length (ranks)
+    } else {
+      index <- as.numeric (num)
+    }
 
-  ## Convert special-case rank values
-  if (num == 'best') {
-    index <- 1
-  } else if (num == 'worst') {
-    index <- length (ranks)
-  } else {
-    index <- as.numeric (num)
+    # Release the result
+    return (data$Hospital.Name[ranks[index]])
   }
   
-  # Release the result
-  return (outcomes$Hospital.Name[ranks[index]])
+  # Rank each state
+  ll <- lapply (split (outcomes, outcomes$State), rankEach)
+  data.frame (hospital=unlist(ll), state=names(ll))
 }
